b18dc253b3b7ea94fd104887cfb951773daeb22e
Yegor <yegor02092002@gmail.com>
2022-11-20 12:01:51 +0200

    Implemented through Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import {
+  fromEvent,
+  Observable,
+  repeatWhen,
+  Subject,
+  switchMap,
+  takeUntil, tap,
+  timer,
+} from 'rxjs';
+
+import { PollingService } from './services/polling.service';
+import { OnlineStatusService, OnlineStatusType } from 'ngx-online-status';
+import { BnNgIdleService } from 'bn-ng-idle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  private stopStream = new Subject();
+  private startStream = new Subject();
+  public observeStream$!: Observable<string>;
+  clickObservable: Observable<Event> = fromEvent(document, 'click');
+
+  status: OnlineStatusType = this.onlineStatusService.getStatus();
+
+  constructor(
+    private pollingService: PollingService,
+    private onlineStatusService: OnlineStatusService,
+    private bnIdle: BnNgIdleService,
+  ) {}
+
+  ngOnInit(): void {
+    this.clickObservable.subscribe(() => {
+      this.start();
+      this.bnIdle.resetTimer();
+    });
+
+    this.bnIdle.startWatching(10).subscribe((isTimedOut: boolean) => {
+      if (isTimedOut) {
+        this.stop();
+        this.bnIdle.stopTimer();
+      }
+    });
+
+    this.onlineStatusService.status.subscribe((status: OnlineStatusType) => {
+      this.status = status;
+      status ? this.start() : this.stop();
+    });
+
+    this.observeStream$ = timer(0, 10000).pipe(
+      switchMap(() => this.pollingService.fetchUrl()),
+      takeUntil(this.stopStream),
+      repeatWhen(() => this.startStream),
+    );
+  }
+
+  ngOnDestroy() {
+    this.startStream.unsubscribe();
+    this.stopStream.unsubscribe();
+  }
+
+  start(): void {
+    this.startStream.next(event);
+  }
+
+  stop(): void {
+    this.stopStream.next(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { OnlineStatusModule } from 'ngx-online-status';
+
+import { BnNgIdleService } from 'bn-ng-idle'; // import bn-ng-idle service
+
+import { MatButtonModule } from '@angular/material/button';
+
+export const materialModules = [MatButtonModule];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, OnlineStatusModule, materialModules],
+  providers: [BnNgIdleService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
